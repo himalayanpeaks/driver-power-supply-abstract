89d405d9ef735653da235288e462ee01bde71570
Rahul <83376754+himalayanpeaks@users.noreply.github.com>
2025-03-30 18:52:41 +0200

    Added view model

---
+++ OneDriver.PowerSupply.Abstract/OneDriver.PowerSupply.Abstract/Contracts/IPowerSupplyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OneDriver.Framework.Module;
+using System.Windows.Input;
+
+namespace OneDriver.PowerSupply.Abstract.Contracts
+{
+    public interface IPowerSupplyViewModel : IDeviceViewModel
+    {
+        ICommand CommandAllChannelsOn { get; }
+        ICommand CommandAllChannelsOff { get; }
+        ICommand CommandSetVolts { get; }
+        ICommand CommandSetAmps { get; }
+    }
+}
